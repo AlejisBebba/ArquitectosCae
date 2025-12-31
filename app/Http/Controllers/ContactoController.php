ae4845a1099eeb19776822210d2981338927e9f5
AlejisBebba <karenandra1717@gmail.com>
2025-12-30 22:38:39 -0500

    Fix contacto

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Mensaje;
-use Illuminate\Support\Facades\Mail;
 
 class ContactoController extends Controller
 {
-    //
+    public function enviar(Request $request)
+{
+    // Esto valida que los campos no lleguen vacíos
+    $request->validate([
+        'nombre' => 'required',
+        'correo' => 'required|email',
+        'mensaje' => 'required',
+    ]);
+
+    // Por ahora, solo retornaremos un mensaje de éxito
+    return back()->with('success', '¡Gracias! Tu mensaje ha sido enviado.');
+}
 }
